19d2ac9c1ec437e2ac6fbcbeebda1e478ef2c2ef
pgh <1762403516@qq.com>
2022-07-13 16:15:05 +0800

    test

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 
-module.exports = function tiny(string) {
+export default function tiny(string) {
     if (typeof string !== "string") throw new TypeError("Tiny wants a string!");
     return string.replace(/\s/g, "");
   };
